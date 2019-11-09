d7fde34c7159a1b910cfea82cc19bc68bd66950d
Gabriel Batista <svbgabriel@gmail.com>
2019-11-09 11:25:31 -0300

    Migra o pedido para Spring

---
+++ src/main/java/br/anhembi/funmodechild/controllers/PedidoController.java
@@ -0,0 +1,37 @@
+package br.anhembi.funmodechild.controllers;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.anhembi.funmodechild.models.Usuario;
+import br.anhembi.funmodechild.repositories.RepositoryPedido;
+import br.anhembi.funmodechild.repositories.RepositoryUsuario;
+
+@Controller
+public class PedidoController {
+
+	@Autowired
+	RepositoryUsuario repositoryUsuario;
+
+	@Autowired
+	RepositoryPedido repositoryPedido;
+
+	@GetMapping("/pedido")
+	public ModelAndView pedido(HttpServletRequest request) {
+		ModelAndView mv = new ModelAndView();
+
+		// Recupera os dados do usuário
+		Principal user = request.getUserPrincipal();
+		Usuario usuario = repositoryUsuario.findByEmail(user.getName());
+
+		mv.setViewName("pedido");
+		mv.addObject("pedidos", repositoryPedido.findByUsuario(usuario.getId()));
+		return mv;
+	}
+}
--- src/main/java/br/anhembi/funmodechild/repositories/RepositoryPedido.java
+++ src/main/java/br/anhembi/funmodechild/repositories/RepositoryPedido.java
@@ -1,9 +1,15 @@
 package br.anhembi.funmodechild.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.anhembi.funmodechild.models.Pedido;
 
 public interface RepositoryPedido extends JpaRepository<Pedido, Long> {
 
+	@Query("SELECT p FROM Pedido p INNER JOIN p.usuario u WHERE u.id = :id")
+	List<Pedido> findByUsuario(@Param("id") long id);
 }
